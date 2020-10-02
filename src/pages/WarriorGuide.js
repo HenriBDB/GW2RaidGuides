7b47dbc50663783ac7283559cd5edbfd13627551
CostaPrimo <jeppeenevold@gmail.com>
2020-10-02 14:26:07 +0200

    more stuff

--- src/pages/WarriorGuide.js
+++ src/pages/WarriorGuide.js
@@ -94,11 +94,37 @@ const WarriorGuide = (props) => {
                         </p>
                     </TabPane>
                     <TabPane tabName="Condi-tb">
+                        <p>
+                            For condition damage you want to run:
+                        </p>
                         <div className="mb-3" data-armory-embed="specializations" data-armory-ids="36,51,18" data-armory-36-traits="1455,1333,1346" data-armory-51-traits="1329,1484,1657" data-armory-18-traits="2039,2042,2038"></div>
-
+                        <p>
+                            This combination provides a lot of condition damage, expertise, and flat duration increases to the main conditions Bleeding and Burning.
+                            The only trait that really stands out from the rest is Burst Mastery as it provides no inherent bonus to condition damage, but the payback on adrenaline helps ensure that the build can generate the adrenaline needed to fire primal bursts off-cooldown.
+                            The rest of the traits have a somewhat direct correlation to condition damage that obviously makes them better than the alternatives.
+                            The only alteration to this build is Sundering Bursts instead of Blademaster for team compositions that struggle with reaching the Vulnerability cap.
+                        </p>
                     </TabPane>
                     <TabPane tabName="Extra-tb">
-
+                        <p>
+                            For Spellbreaker you want to run:
+                        </p>
+                        <div className="mb-3" data-armory-embed="specializations" data-armory-ids="4,51,61" data-armory-4-traits="1444,1338,1437" data-armory-51-traits="1413,1484,1369" data-armory-61-traits="2095,2060,2107"></div>
+                        <p>
+                            The goal here is to achieve as many offensive modifiers as possible, which is sadly lacking amongst the master traits.
+                            The corresponding rotation to this setup aims to keep Attacker’s Insight stacked as much as possible while pressing big damage skills such as Whirling Axe while most modifiers are active.
+                            It is as mentioned possible to run Loss Aversion over Sun and Moon Style, but it is only worthwhile on bosses where regular boon application are prominent (I.E. Qadim).
+                        </p>
+                        <br/>
+                        <p>
+                            For hand-kiting you want to run:
+                        </p>
+                        <div className="mb-3" data-armory-embed="specializations" data-armory-ids="4,22,61" data-armory-4-traits="1451,1449,1454" data-armory-22-traits="1376,1368,1649" data-armory-61-traits="2153"></div>
+                        <p>
+                            The goal here is to maximise defensive capabilities along with access to sustain.
+                            The most important part about hand kiting is either blocking the damage from hands or outhealing it, and this build attempts to do a mix of both, changing between healing through the hands and blocking them, while still having blocks leftover for mind crush.
+                            The last 2 slots in Spellbreaker are since there are no real good options that especially benefit hand-kiting, so it is possible to pick whatever for those without losing out on any benefits since the trait-line is taken only for access to Full Counter.
+                        </p>
                     </TabPane>
                 </NavTabs>
 
