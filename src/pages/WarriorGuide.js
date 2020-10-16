e08f02c5fc7f9ab7c129029e7b17e7442df21122
paulakarolak <paula@karolak.pl>
2020-10-16 17:04:39 +0200

    fixed wariorr styles

--- src/pages/WarriorGuide.js
+++ src/pages/WarriorGuide.js
@@ -11,7 +11,7 @@ import {Healing, Banners, Physical, Rage, Shout, Signet, Stance, Elite, Meditati
 const WarriorGuide = (props) => {
     return (
         <div className={"warrior-page pt-5"}>
-            <div className="guide-content">
+            <div className="guide-content warrior">
                 <GuideTitle title="Flames of War" footer="King of Fires Edition"/>
                 <br/>
                 <p>
