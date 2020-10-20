c0d84478924c2ee355eaf807de2d74439f6e3c0e
paulakarolak <paula@karolak.pl>
2020-10-20 18:24:07 +0200

    finished styling Zeal traits

--- src/components/Dragonhunter/Traits/DragonhunterTrait.js
+++ src/components/Dragonhunter/Traits/DragonhunterTrait.js
@@ -15,60 +15,60 @@ const DragonhunterTrait = () => {
             </div>
             <TraitCard traitID={648} gameClass="dragonhunter">
                 <p>
-                    Casts a Lesser Symbol of Wrath when you are struck while below 75% health.
+                    Casts a <span data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="13677" className="dhskill"></span> when you are struck while below 75% health.
                 </p>
                 <p>
-                    Grants <WikiIcon name={"Retaliation"} /> Retaliation (2s) to you and your allies standing in the symbol (5 targets). It's also a Light Field.
+                    Grants <WikiIcon name={"Retaliation"} /> Retaliation (2s) to you and your allies standing in the symbol (5 targets). It's also a <a href="https://wiki.guildwars2.com/wiki/Light_field" target="_blank" className="dhlink">Light Field</a>.
                 </p>
             </TraitCard>
             <div className="row justify-content-center">
                 <h4 className="mb-0">Major Adept</h4>
             </div>
-            <TraitCard traitID={634}>
+            <TraitCard traitID={634} gameClass="dragonhunter">
                 <p>
-                    Increases direct damage dealt to burning enemies by 7%.
+                    Increases direct damage dealt to enemies with <WikiIcon name="Burning"/> Burning by 7%.
                 </p>
 
                 <p>
-                    You should always have <WikiIcon name={"Burning"} /> Burning on the boss, for example from your <WikiIcon name={"SpearOfJustice"} /> Spear of Justice, from druid's Sun Spirit, firebrand's Flame Mantra, etc. This trait is a pure damage increase and it's the most worth taking from Major Adept set.
+                    You should always have <WikiIcon name={"Burning"} /> Burning on the boss, for example from your <span data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="29887" className="dhskill"></span>, from druid's Sun Spirit, firebrand's Flame Mantra, etc. This trait is a pure damage increase and it's the most worth taking from Major Adept set.
                 </p>
             </TraitCard>
             <div className="row justify-content-center">
                 <h4 className="mb-0">Minor Master</h4>
             </div>
-            <TraitCard traitID={646}>
+            <TraitCard traitID={646} gameClass="dragonhunter">
                 <p>
                     This trait makes your Symbols inflict <WikiIcon name={"Vulnerability"} /> Vulnerability on foes. Damage to vulnerable enemies is increased by 5%.
                 </p>
                 <p>
-                    You should always have <WikiIcon name={"Vulnerability"} /> Vulnerability on the boss, for example from your Symbols, <WikiIcon name={"SpearOfJustice"} /> Spear of Justice, <WikiIcon name={"SwordOfJustice"} /> Sword of Justice and from multiple skills from other classes.
+                    You should always have <WikiIcon name={"Vulnerability"} /> Vulnerability on the boss, for example from your Symbols, <span data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="29887" className="dhskill"></span>, <span data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="9168" className="dhskill"></span> and from multiple skills from other classes.
                </p>
             </TraitCard>
             <div className="row justify-content-center">
                 <h4 className="mb-0">Major Master</h4>
             </div>
-            <TraitCard traitID={653}>
+            <TraitCard traitID={653} gameClass="dragonhunter">
                 <p>
-                    Grants 120 additional power while wielding a Greatsword and reduces the cooldown of Greatsword skills by 20%. This trait is the main reason why Greatsword is the go-to weapon for a power Dragonhunter.
+                    Grants 120 additional <WikiIcon name={"Power"} /> Power (on any weaponset) and additional 120 <WikiIcon name={"Power"} /> Power while wielding a Greatsword (so 240 more <WikiIcon name={"Power"} /> Power in total) and reduces the <WikiIcon name={"Recharge"} /> cooldown of Greatsword skills by 20%. This trait is the main reason why Greatsword is the go-to weapon for a power Dragonhunter.
                 </p>
             </TraitCard>
             <div className="row justify-content-center">
                 <h4 className="mb-0">Minor Grandmaster</h4>
             </div>
-            <TraitCard traitID={649}>
+            <TraitCard traitID={649} gameClass="dragonhunter">
                 <p>
-                    This trait makes your Symbols deal 30% more damage.
+                    This trait makes your Symbols deal 30% more damage and charges your <span data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="29887" className="dhskill"></span> passive twice as fast.
                 </p>
             </TraitCard>
             <div className="row justify-content-center">
                 <h4 className="mb-0">Major Grandmaster</h4>
             </div>
-            <TraitCard traitID={2017}>
+            <TraitCard traitID={2017} gameClass="dragonhunter">
                 <p>
                     Increases your damage by 2% for 15s whenever your Symbol hits a foe - you can have max 5 stacks, which results in 10% damage increase.
                 </p>
                 <p>
-                    You should be using Symbols as much as possible in your rotation to upkeep the 5 stacks of Symbolic Avenger for the permanent 10% damage increase.
+                    You should be using Symbols as much as possible in your rotation to upkeep the 5 stacks of <span data-armory-embed="traits" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="2017" className="dhskill"></span> for the permanent 10% damage increase.
                 </p>
             </TraitCard>
         </>
