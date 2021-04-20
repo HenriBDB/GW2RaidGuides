ef3739b4797b0adea5983ddabc8b79d7c29515f4
Henri <henri.dbelloy@gmail.com>
2021-04-20 18:44:29 +0100

    Add druid CC section.

---
+++ src/components/Druid/Sections/CC/WeaponCC.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import WikiIcon from '../../../WikiIcon'
+
+const WeaponCC = () => {
+    return (
+        <>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12508"></div>
+                <div class="col-lg-2">200cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Daze"/> Daze or <WikiIcon name="Stun"/> Stun (2s)
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="31318"></div>
+                <div class="col-lg-2">150cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Daze"/> Daze (1.5s)
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12511"></div>
+                <div class="col-lg-2">150cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Knockback"/> Knockback
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12638"></div>
+                <div class="col-lg-2">150cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Pull"/> Pull
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12475"></div>
+                <div class="col-lg-2">150cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Daze"/> Daze or <WikiIcon name="Stun"/> Stun (1.5s)
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12523"></div>
+                <div class="col-lg-2">150cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Knockback"/> Knockback
+                </p>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12485"></div>
+                <div class="col-lg-2">100cc</div>
+                <p class="col-lg-7">
+                    <WikiIcon name="Daze"/> Daze (1s)
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default WeaponCC
