b59ab8adec157bcdd59310cf7a025cca20e3cb49
HenriBDB <44209139+HenriBDB@users.noreply.github.com>
2021-04-25 14:28:20 +0100

    Fix incorrect cc values for Pink Moa

--- src/components/Druid/Sections/CC/PetCC.js
+++ src/components/Druid/Sections/CC/PetCC.js
@@ -39,7 +39,7 @@ const PetCC = () => {
             <div class="row mt-3">
                 <div class="col-lg-12"><WikiIcon name="PinkMoa" size={30}/> Pink Moa</div>
                 <div class="col-lg-3" data-armory-embed="skills" data-armory-size="20" data-armory-inline-text="wiki" data-armory-ids="12708"></div>
-                <div class="col-lg-2">100cc</div>
+                <div class="col-lg-2">200cc</div>
                 <p class="col-lg-7">
                     <WikiIcon name="Daze"/> Daze (2s)
                 </p>
